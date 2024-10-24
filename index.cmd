7ab5570a698c35c26701f9a3e981a079f5a5c971
Conway <yawnoc@pm.me>
2024-10-25 06:55:09 +0800

    Link to submodule CSS

--- index.cmd
+++ index.cmd
@@ -10,6 +10,7 @@ OrdinaryDictionaryReplacement: #.boilerplate-properties-override
     <link rel="shortcut icon" href="/favicon.ico">
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="manifest" href="/site.webmanifest">
+    <link rel="stylesheet" href="/javascript/stroke-input.min.css">
 
 RegexDictionaryReplacement: #.chinese-lang
 - queue_position: BEFORE #escape-idle-html
